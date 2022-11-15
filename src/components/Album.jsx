f6fa3decd7d3cae5199081caaf6ffa27ab512683
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-11-15 21:19:36 +0100

    build: package fix

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AudioPlayer from 'material-ui-audio-player';
+// import AudioPlayer from 'material-ui-audio-player';
 
 import dreamCity from '/assets/dream-city.mp4';
 import pic1 from '/assets/image1.png';
@@ -167,9 +167,9 @@ export default function Album() {
                     </Container>
                 </Box>
 
-                <Container sx={{ py: 8 }} maxWidth="xl">
+                {/* <Container sx={{ py: 8 }} maxWidth="xl">
                     <AudioPlayer src={dreamCity} />
-                </Container>
+                </Container> */}
 
                 {/* container with margin bottom */}
                 <Container maxWidth="xl" sx={{ pb: 4 }}>
